25838f165e0f8c54cfd280d08fdeeffadc3eb07e
Le Linh <linhld@hachitechsolution.com>
2024-10-29 16:57:56 +0700

    search rooms

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->get('/example', function (Request $request) {
     return response()->json(['message' => 'This is an example API response.']);
 });
+
+Route::get('/search', [RoomController::class, 'search'])
+    ->name('search');
