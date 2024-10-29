d94a75634892d7339204547c0d675588b49868bc
dat <dat277@gmail.com>
2024-10-29 22:49:48 +0700

    fix: run serve

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';  // Chỉ giữ import này.
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths"
-import { configDefaults, defineConfig } from "vitest/config"
-
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults } from 'vitest/config';  // Chỉ lấy các config cần.
 
 export default defineConfig({
-  plugins: [laravel(['resources/js/app.tsx', 'resources/css/app.css']), react()],
+  plugins: [
+    laravel(['resources/js/app.tsx', 'resources/css/app.css']),
+    react(),
+    tsconfigPaths()
+  ],
   build: {
     manifest: true,
     outDir: 'public/build',
-},
-test: {
-  exclude: [
-    ...configDefaults.exclude,
-    "**/node_modules/**",
-    "**/fixtures/**",
-  ],
-},
-plugins: [tsconfigPaths()],
+  },
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      '**/node_modules/**',
+      '**/fixtures/**',
+    ],
+  },
 });
