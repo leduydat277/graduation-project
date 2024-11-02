3e49df9bad09cccc1bb3531934d899940ac79b8e
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-11-02 20:55:52 +0700

    fix route

--- routes/admin_feature/CRUD_User.php
+++ routes/admin_feature/CRUD_User.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Admin\UserController;
 // Nhóm route với prefix 'admin/users'
 Route::prefix('admin/users')->group(function () {
     // Định nghĩa route cho hành động test trong UserController
-    Route::get('test', [UserController::class, 'test_function']);
+    Route::get('test', [UserController::class, 'test_function'])->name('test');
 });
