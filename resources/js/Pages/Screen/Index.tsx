c8fd070f475611131c530beec817cb159195884c
dat <dat277@gmail.com>
2024-11-06 11:06:26 +0700

    feat: add file

--- resources/js/Pages/Screen/Index.tsx
+++ resources/js/Pages/Screen/Index.tsx
@@ -1,17 +1,19 @@
 import { Link } from '@inertiajs/react';
 import MainLayout from '@/Layouts/MainLayout';
 import { RoomSearchBar } from './../../shared/RoomSearchBar';
-
+import { RoomItem } from '../../shared/Room/RoomItem';
+import { RoomList } from '../../shared/Room/RoomList';
 import { Box } from '@mui/material';
 import { grey, pink } from '@mui/material/colors';
+import '@fontsource/roboto/300.css';
 
 
 function ScreenPage() {
   return (
     <>
-
       <Box sx={{ backgroundColor: pink[200] }} >
-        <RoomSearchBar />
+      <RoomList />
+      
       </Box>
 
     </>
