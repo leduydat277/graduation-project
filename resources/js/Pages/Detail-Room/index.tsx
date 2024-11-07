a34a4272fb43017eb67fa15f03c4bc8f55dbda50
Hoang Huong <thuy.huong.hoang@draphony.com>
2024-11-07 10:25:54 +0700

    tao pull req

--- resources/js/Pages/Detail-Room/index.tsx
+++ resources/js/Pages/Detail-Room/index.tsx
@@ -3,14 +3,6 @@ import MainLayout from '@/Layouts/MainLayout';
 import { RoomSearchBar } from './../../shared/RoomSearchBar';
 
 import React from 'react';
-import { Box, Button, Container, Grid, Typography, Paper, InputAdornment, TextField, Select, MenuItem, InputLabel, FormControl, Stack, Card, CardMedia, CardContent, Chip } from '@mui/material';
-import { blue, grey } from '@mui/material/colors';
-import SearchIcon from '@mui/icons-material/Search';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import GroupIcon from '@mui/icons-material/Group';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import LuggageIcon from '@mui/icons-material/Luggage';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 
 function DetailRoom() {
