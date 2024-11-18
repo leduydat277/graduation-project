a4f1233fde4bc4f6fdf1302f1648e4017aa8f309
Bùi Đức Ninh <145408337+duckningmoi@users.noreply.github.com>
2024-11-18 17:19:50 +0700

    done api user xem lịch sử hóa đơn và tìm kiếm hóa đơn theo ngày

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -9,33 +9,146 @@ class PaymentController
 {
     public function allPayments()
     {
-        $user = Auth::user();
+        // $user = Auth::user();
+        $user = 10;
+        $payments = Payment::with([
+            'booking' => function ($query) {
+                $query->select('id', 'tien_coc as tiencoc', 'total_price as tienphong', 'status as status');
+            },
+            'booking.phiphatsinhs' => function ($query) {
+                $query->select('id', 'booking_id', 'name', 'price');
+            }
+        ])
+            ->whereHas('booking', function ($query) use ($user) {
+                $query->where('user_id', $user);
+            })
+            ->get()
+            ->groupBy('booking.id')
+            ->map(function ($paymentGroup) {
+                $totalAmount = $paymentGroup->sum('total_price');
+                $paymentCount = $paymentGroup->count();
+                $paymentPrices = $paymentGroup->pluck('total_price');
+                $phiphatsinhs = $paymentGroup->first()->booking->phiPhatSinhs->map(function ($item) {
+                    return [
+                        'dichvuphatsinh' => $item->name,
+                        'tienphatsinh' => $item->price
+                    ];
+                });
+                $paymentStatusLabel = '';
+                switch ($paymentGroup->first()->payment_status) {
+                    case 0:
+                        $paymentStatusLabel = 'Chưa thanh toán cọc';
+                        break;
+                    case 1:
+                        $paymentStatusLabel = 'Đang thanh toán';
+                        break;
+                    case 2:
+                        $paymentStatusLabel = 'Đã thanh toán cọc';
+                        break;
+                    case 3:
+                        $paymentStatusLabel = 'Đã thanh toán tổng tiền đơn';
+                        break;
+                    case 4:
+                        $paymentStatusLabel = 'Đã hủy';
+                        break;
+                    default:
+                        $paymentStatusLabel = 'Không xác định';
+                }
+                return [
+                    'tongtiendathanhtoan' => $totalAmount,
+                    'solanthanhtoan' => $paymentCount,
+                    'tiencoc' => $paymentGroup->first()->booking->tiencoc ?? null,
+                    'tienphong' => $paymentGroup->first()->booking->tienphong ?? null,
+                    'phiphatsinh' => $phiphatsinhs,
+                    'trangthai' => $paymentStatusLabel ?? null,
+                    'sotienmoilanthanhtoan' => $paymentPrices,
+                ];
+            });
+            if ($payments->isEmpty()) {
+                return response()->json([
+                    "code" => 401,
+                    "message" => "Bạn chưa có hóa đơn nào!",
+                ]);
+            }
         return response()->json([
             "code" => 200,
             "message" => "Get success",
-            "data" => $user
-        ]);
-        $payments = Payment::all();
-        $data = $payments->map(function ($payment) {
-            return [
-                'id' => $payment->id,
-                'booking_id' => $payment->booking_id,
-                'payment_date' => $payment->payment_date,
-                'total_price' => $payment->total_price,
-            ];
-        });
-        return response()->json([
-            "code" => 200,
-            "message" => "Get success",
-            "data" => $data
+            "data" => $payments
         ]);
     }
 
-    public function filterPayments(){
-        $fromDate = strtotime(request()->input('fromDate'));
-        $toDate = strtotime(request()->input('toDate'));
-        $payments = Payment::whereBetween('payment_date', [$fromDate, $toDate])->get();
-
+    public function filterPayments()
+    {
+        $fromDate = strtotime(request()->input('fromDate', '')); 
+        $toDate = strtotime(request()->input('toDate', ''));      
+        if (!$fromDate || !$toDate) {
+            return response()->json([
+                "code" => 400,
+                "message" => "Vui lòng nhập theo định dạng d-M-Y",
+            ]);
+        }
+        // $user = Auth::user();
+        $user = 10; 
+        $payments = Payment::with([
+            'booking' => function ($query) {
+                $query->select('id', 'tien_coc as tiencoc', 'total_price as tienphong', 'status as status');
+            },
+            'booking.phiphatsinhs' => function ($query) {
+                $query->select('id', 'booking_id', 'name', 'price');
+            }
+        ])
+            ->whereHas('booking', function ($query) use ($user) {
+                $query->where('user_id', $user);
+            })
+            ->whereBetween('payment_date', [$fromDate, $toDate])  
+            ->get()
+            ->groupBy('booking.id')
+            ->map(function ($paymentGroup) {
+                $totalAmount = $paymentGroup->sum('total_price'); 
+                $paymentCount = $paymentGroup->count();  
+                $paymentPrices = $paymentGroup->pluck('total_price');  
+                $phiphatsinhs = $paymentGroup->first()->booking->phiPhatSinhs->map(function ($item) {
+                    return [
+                        'dichvuphatsinh' => $item->name,
+                        'tienphatsinh' => $item->price
+                    ];
+                });
+                $paymentStatusLabel = '';
+                switch ($paymentGroup->first()->payment_status) {
+                    case 0:
+                        $paymentStatusLabel = 'Chưa thanh toán cọc';
+                        break;
+                    case 1:
+                        $paymentStatusLabel = 'Đang thanh toán';
+                        break;
+                    case 2:
+                        $paymentStatusLabel = 'Đã thanh toán cọc';
+                        break;
+                    case 3:
+                        $paymentStatusLabel = 'Đã thanh toán tổng tiền đơn';
+                        break;
+                    case 4:
+                        $paymentStatusLabel = 'Đã hủy';
+                        break;
+                    default:
+                        $paymentStatusLabel = 'Không xác định';
+                }
+                return [
+                    'tongtiendathanhtoan' => $totalAmount,  
+                    'solanthanhtoan' => $paymentCount,  
+                    'tiencoc' => $paymentGroup->first()->booking->tiencoc ?? null, 
+                    'tienphong' => $paymentGroup->first()->booking->tienphong ?? null, 
+                    'phiphatsinh' => $phiphatsinhs,
+                    'trangthai' => $paymentStatusLabel, 
+                    'sotienmoilanthanhtoan' => $paymentPrices,  
+                ];
+            });
+            if($payments->isEmpty()) {
+                return response()->json([
+                    "code" => 401,
+                    "message" => "Không tìm thấy hóa đơn nào!",
+                ]);
+            }
         return response()->json([
             "code" => 200,
             "message" => "Filter success",
