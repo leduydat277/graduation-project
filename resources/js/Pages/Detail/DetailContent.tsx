42e72b1d03ca2aff0859019e5fa61286c4438e51
dat <dat277@gmail.com>
2024-12-03 22:29:29 +0700

    fix: ui

--- resources/js/Pages/Detail/DetailContent.tsx
+++ resources/js/Pages/Detail/DetailContent.tsx
@@ -1,13 +1,21 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Box } from "@mui/material";
 import HomeBannner5 from '../../../assets/HomeBanner5.jpg';
 import {formatPrice} from '../../../service/hooks/price'
 import { CarouselCustom } from '@/shared/CarouselCustom';
+import { useBookingStore } from '../../../service/stores/booking-store';
 
 export const DetailContent = (props) => {
-  const { title, description, subtitle='Trải nghiệm sang trọng và thoải mái bậc nhất', image = '', price } = props
-    console.log('DetailContent', props)
+console.log('props', props)
+  const { title, description, subtitle='Trải nghiệm sang trọng và thoải mái bậc nhất', price, image_room } = props
+  console.log('image_room', image_room)
+
+ const SLEEP_HOTTEL_URl_IMAGE = 'http://127.0.0.1:8000/storage/'
+  const [setPrice] = useBookingStore((state) => [state.setPrice]);
+  useEffect(() => {
+    setPrice(price);
+  }, [price, setPrice]);
 
     const items = [
       { src: "https://via.placeholder.com/600x400?text=Image+1", alt: "Image 1" },
@@ -18,6 +26,7 @@ export const DetailContent = (props) => {
     ];
   return (
     <Box sx={{ minHeight: '300px'}}>
+      
                   <CarouselCustom items={items}  />
               <Typography variant="h3" pt={4}> {title}</Typography>
             
