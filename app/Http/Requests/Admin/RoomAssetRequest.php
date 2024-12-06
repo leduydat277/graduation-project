8caea9d3ef17f8ca38716a8a66f4560ac812adcb
Le Linh <80584486+linhle1913@users.noreply.github.com>
2024-12-06 15:53:27 +0700

    done fake data

--- app/Http/Requests/Admin/RoomAssetRequest.php
+++ app/Http/Requests/Admin/RoomAssetRequest.php
@@ -24,7 +24,6 @@ class RoomAssetRequest extends FormRequest
         return [
             'room_id' => 'required|exists:rooms,id', // Phải có và tồn tại trong bảng rooms
             'assets_type_id' => 'required|exists:assets_types,id', // Phải có và tồn tại trong bảng assets_types
-            'status' => 'required|in:0,1', // Phải có và là 0 hoặc 1
         ];
     }
 
@@ -40,8 +39,6 @@ class RoomAssetRequest extends FormRequest
             'room_id.exists' => 'Phòng không tồn tại.',
             'assets_type_id.required' => 'Vui lòng chọn loại tiện nghi.',
             'assets_type_id.exists' => 'Loại tiện nghi không tồn tại.',
-            'status.required' => 'Vui lòng chọn trạng thái.',
-            'status.in' => 'Trạng thái không hợp lệ. Vui lòng chọn Đang sử dụng hoặc Tạm dừng sử dụng.',
         ];
     }
 }
