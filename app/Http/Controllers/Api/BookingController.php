3b84c9f523f9288cc295a8000cb8c15021e4e7c7
Le Linh <80584486+linhle1913@users.noreply.github.com>
2024-12-15 22:55:46 +0700

    fix notification

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -440,7 +440,7 @@ class BookingController
             ];
 
             Notification::create([
-                "user_id" => $request->user()->id,
+                "user_id" => Auth::user()->id,
                 "title" => $title,
                 "message" => json_encode($messageData, JSON_UNESCAPED_UNICODE)
             ]);
