d17935f3603d0cb951c31595e5bdf9b3fb0463a5
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-12-18 21:05:45 +0700

    fix rollback manage_status_room when reject booking

--- app/Http/Controllers/Admin/ManageStatusRoomController.php
+++ app/Http/Controllers/Admin/ManageStatusRoomController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Admin\ManageStatusRoom;
 use App\Models\Admin\Room;
+use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Log\Logger;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ManageStatusRoomController extends Controller
@@ -161,8 +163,43 @@ class ManageStatusRoomController extends Controller
         return 'Thêm bản ghi vào bảng Manage Status Room thành công';
     }
 
-    public function cancel(){
-        $booking_id = 263;
-        $record = ManageStatusRoom::where('booing')->first();
+    public static function cancel($booking_id)
+    {
+        $record = ManageStatusRoom::where('booking_id', '=', $booking_id)->first();
+
+        $recentToPrevious =  (new DateTime())->setTimestamp($record->from)->setTime(12, 0, 0)->getTimestamp();
+        $recentToNext =  (new DateTime())->setTimestamp($record->to)->setTime(14, 0, 0)->getTimestamp();
+
+        $recordPrevious = ManageStatusRoom::where([
+            ['to', '=', $recentToPrevious],
+            ['room_id', '=', $record->room_id],
+        ])->first() ?? '';
+
+        $recordNext = ManageStatusRoom::where([
+            ['from', '=', $recentToNext],
+            ['room_id', '=', $record->room_id],
+        ])->first() ?? '';
+
+        if (!$recordPrevious) {
+            $newFrom = $record->from;
+        } else {
+            $recordPrevious->delete();
+            $newFrom = $recordPrevious->from;
+        }
+        if (!$recordNext) {
+            $newTo = $record->to;
+        } else {
+            $recordNext->delete();
+            $newTo = $recordNext->to;
+        }
+
+        $record->update([
+            'from' => $newFrom,
+            'to' => $newTo,
+            'status' => 1,
+            'booking_id' => null,
+        ]);
+
+        return true;
     }
 }
